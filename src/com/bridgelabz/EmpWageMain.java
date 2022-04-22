04d70121fbc042a871786d222a2e0cf9f176b52f
iaksharhate <iaksharhate@gmail.com>
2022-04-22 16:06:31 +0530

    UC2: Checking employess daily wage.

--- src/com/bridgelabz/EmpWageMain.java
+++ src/com/bridgelabz/EmpWageMain.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class EmpWageMain {
     public static void main(String[] args) {
 
-        EmpWageOops empWageOops = new EmpWageOops();
-        empWageOops.empAttendance();
+        EmpWageOops dailyEmpWage = new EmpWageOops();
+        dailyEmpWage.dailyEmpWage();
     }
 }
--- src/com/bridgelabz/EmpWageOops.java
+++ src/com/bridgelabz/EmpWageOops.java
@@ -5,6 +5,10 @@ public class EmpWageOops {
     public static final int empAbsent = 0;
     public static final int empFullTime = 1;
     public static final int empPartTime = 2;
+    public static final int empRatePerHour = 20;
+
+    public static int empWage = 0;
+    public static int empHours = 0;
 
     public static boolean empAttendance(){
 
@@ -19,5 +23,21 @@ public class EmpWageOops {
             System.out.println("Employee is present!");
             return true;
         }
+    }
+    public static boolean dailyEmpWage(){
+
+        boolean status;
+
+        status = empAttendance();
+        if (status){
+            empHours = 8;
+            empWage = empHours * empRatePerHour;
+            System.out.println(empWage);
+        }else{
+            empHours = 0;
+            empWage = 0;
+            System.out.println(empWage);
+        }
+        return true;
     }
 }
